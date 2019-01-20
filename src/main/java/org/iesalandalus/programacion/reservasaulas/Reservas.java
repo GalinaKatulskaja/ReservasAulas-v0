e1550d32b7484c2858b02ee2550d9b0086b6f110
Galina K. <galinakatulskaja1228@list.ru>
2019-01-20 10:30:38 +0100

    5)Comienzo clase Reservas

---
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/Reservas.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.iesalandalus.programacion.reservasaulas;
+
+/**
+ *
+ * @author Galina
+ */
+public class Reservas {
+    private static final int MAX_RESERVAS = 10;
+    private int numReservas;
+    
+    public Reservas(){
+    }
+    public Reservas(Reservas r){
+    }
+
+    public int getNumReservas() {
+        return numReservas;
+    }
+    private void setReservas(){
+    }
+
+    public void setNumReservas(int numReservas) {
+        this.numReservas = numReservas;
+    }
+    
+    
+}
