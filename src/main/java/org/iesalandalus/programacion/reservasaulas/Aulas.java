f43e239c822873473f35f48f59fa86e46e60982a
Galina K. <galinakatulskaja1228@list.ru>
2019-01-20 10:41:52 +0100

    5)Comienzo clase Aulas

---
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/Aulas.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.iesalandalus.programacion.reservasaulas;
+
+/**
+ *
+ * @author Galina
+ */
+public class Aulas {
+    private static final int MAX_AULAS=10;
+    private int numAulas;
+    
+    public Aulas(){}
+    
+    public Aulas(Aulas a){}
+
+    public int getNumAulas() {
+        return numAulas;
+    }
+
+    public void setNumAulas(int numAulas) {
+        this.numAulas = numAulas;
+    }
+    
+    
+    
+}
