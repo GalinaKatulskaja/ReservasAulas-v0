42857a788c8d8ba0fd8b4600fc2cc87647027da1
Galina K. <galinakatulskaja1228@list.ru>
2019-01-21 16:11:53 +0100

    5)Creo la clase Aula

---
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/Aula.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.iesalandalus.programacion.reservasaulas;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Galina
+ */
+public class Aula {
+    private String nombre;
+    
+    public Aula(String nombre){
+        setNombre(nombre);
+        
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    private void setNombre(String nombre) {
+        if(nombre==null){
+            throw new IllegalArgumentException("El nombre del aula no puede ser nulo."); }
+        if(nombre.equals("")){
+            throw new IllegalArgumentException("El nombre del aula no puede estar vacío.");
+        }
+        this.nombre = nombre;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 31 * hash + Objects.hashCode(this.nombre);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Aula other = (Aula) obj;
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        //("[nombre=Salón de actos]", aula.toString())
+        return  "[nombre=" + nombre +"]";
+    }
+    
+    
+}   
