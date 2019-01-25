e141eae5a0cdc605fb5dc9a2bd7fdbf9633bce38
Galina K. <galinakatulskaja1228@list.ru>
2019-01-25 17:30:08 +0100

    Agrego cambio en la clase Aula, el constructor copia

--- src/main/java/org/iesalandalus/programacion/reservasaulas/Aulas.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/Aulas.java
@@ -5,6 +5,8 @@
  */
 package org.iesalandalus.programacion.reservasaulas;
 
+import javax.naming.OperationNotSupportedException;
+
 /**
  *
  * @author Galina
@@ -12,35 +14,72 @@ package org.iesalandalus.programacion.reservasaulas;
 public class Aulas {
     private static final int MAX_AULAS=10;
     private int numAulas;
+    private Aula[] coleccionAulas;
     
-    public Aulas(){}
+    public Aulas(){
+        this.numAulas = 0;
+        this.coleccionAulas = new Aula[MAX_AULAS];
+    }
     
     public Aulas(Aulas aulas){
+        setAulas(aulas);
     }
 
     public int getNumAulas() {
         return numAulas;
     }
 
-    public void setNumAulas(int numAulas) {
-       
-        this.numAulas = numAulas;
+    public void setAulas(Aulas aulas) {
+        if(aulas == null){
+            throw new IllegalArgumentException("No se pueden copiar aulas nulas.");
+        }
+        this.coleccionAulas = this.copiaProfundaAulas(aulas.coleccionAulas);      
+        this.numAulas = aulas.numAulas;
     }
-    privado Aula copiaProfundaAulas(Aula[] a){
-        Aula[] otrasAulas = new Aula[aulas.length];
-		for (int i = 0; i < aulas.length && aulas[i] != null; i++) {
-			otrasAulas[i] = new Aula(aulas[i]);
+    private Aula[] copiaProfundaAulas(Aula[] coleccionAulas){
+        Aula[] otrasAulas = new Aula[coleccionAulas.length];
+		for (int i = 0; i < coleccionAulas.length && coleccionAulas[i] != null; i++) {
+			otrasAulas[i] = new Aula(coleccionAulas[i]);
 		}
 		return otrasAulas;
     
     }
-    public Aulas[] getAulas(){
-    return Aulas[];
+    public Aula[] getAulas(){
+        return  copiaProfundaAulas(coleccionAulas);
     }
-    public void insetrar(Aula aula){
-        
+    public int getNumAulas(){
+        return numAulas;
     }
-    private int buscarIndiceAula(Aula){
+    public void insetrar(Aula aula) throws OperationNotSupportedException{
+        if( aula == null){
+            throw new IllegalArgumentException("No se puede insertar un aula nula.");
+        }
+        int indice = buscarIndiceAula(aula);
+        if(indiceNoSuperaTamano(indice)== false) {
+            coleccionAulas[indice] = aula;
+            numAulas=+1;
+        }
+        else {
+            if(indiceNoSuperaCapacidad(indice)){
+                throw new OperationNotSupportedException("El aula ya existe.");
+            }
+            else {
+                throw new OperationNotSupportedException("");
+            }
+        }
+    }
+    private int buscarIndiceAula(Aula aula){
+        int indice = 0;
+        boolean encontrado = false;
+        if(indiceNoSuperaTamano(indice) && encontrado == false){
+            if(coleccionAulas[indice].equals(aula)){
+                encontrado= true;
+            }
+            else {
+                indice=+1;
+            }
+            return indice;
+        }
     }
     private boolean indiceNoSuperaTamano(int){
     }
